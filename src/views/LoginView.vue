93f8eab459e7954c134ce9207a190fb73c6f4269
Schmid Maximilian, GHR-OSA-NEX-31 <Maxi03milian@gmail.com>
2022-10-27 16:06:52 +0200

    added focus animation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="login">
-    <div class="login-title">
+    <div class="login-title" ref="title">
       <h1>Supervisor</h1>
       <p>Bringing out the best <br />of your work</p>
     </div>
-    <div class="login-form">
+    <div class="login-form" ref="form">
       <h4 class="form-title"><b>Log in</b></h4>
-      <v-text-field label="Email" color="#ffffff" outlined></v-text-field>
+      <v-text-field
+        @focus="focusInputs"
+        @blur="unfocusInputs"
+        label="Email"
+        color="#ffffff"
+        outlined
+      ></v-text-field>
       <v-text-field label="Password" color="#ffffff" outlined></v-text-field>
-      <v-btn class="form-button" elevation="10" color="#193864" block rounded>Log in</v-btn>
+      <v-btn class="form-button" elevation="10" color="#193864" block rounded
+        >Log in</v-btn
+      >
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   name: "HomeView",
   components: {},
+  setup() {
+    const title = ref(null);
+    const form = ref(null);
+    function focusInputs() {
+      title.value.classList.add("title-move");
+      form.value.classList.add("form-move");
+    }
+    function unfocusInputs() {
+      title.value.classList.remove("title-move");
+      form.value.classList.remove("form-move");
+    }
+    return {
+      focusInputs,
+      unfocusInputs,
+      title,
+      form
+    };
+  },
 };
 </script>
 
@@ -27,16 +54,29 @@ export default {
 }
 .login-title {
   font-size: 1.5rem;
+  transition: all 0.25s ease;
 }
 .login-form {
   margin-top: 2rem;
+  transition: all 0.25s ease;
 }
 
 .form-title {
   margin-bottom: 1rem;
 }
 
-.form-button{
+.form-button {
   height: 3rem;
+}
+
+.title-move {
+  opacity: 0;
+  transform: translateY(-100%);
+  transition: all 0.25s ease;
+}
+
+.form-move{
+  transform: translateY(-80%);
+  transition: all 0.25s ease;
 }
 </style>

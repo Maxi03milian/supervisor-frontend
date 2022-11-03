a777889495fa58af92fd56ce70db819de46f2272
Schmid Maximilian, GHR-OSA-NEX-31 <Maxi03milian@gmail.com>
2022-11-03 17:01:24 +0100

    created base navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="app">
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
 export default {
   name: 'App',
   components: {
+    Navbar
   },
   setup() {
     document.getElementsByTagName('html')[0].classList.add('app-background')
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-left">
+
+    </div>
+    <div class="navbar-center" >
+        Good morning Max
+    </div>
+    <div class="navbar-right" >
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 5rem;
+  width: 100%;
+  color: #ffffff;
+  padding: 0 20px;
+  font-weight: 600;
+}
+
+</style>
